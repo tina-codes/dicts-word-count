e478099a5b9dca3c7a98660942e7d4ae6dd67db9
Christina <you@example.comchristinanicosia08@gmail.comgit config --global user.name Your>
2022-09-26 12:24:39 -0700

    count_words function working on twain.txt

--- wordcount.py
+++ wordcount.py
@@ -6,13 +6,15 @@ def count_words(input_file):
     word_count = {}
 
     for line in input_file:
-        
-        words = line.rstrip().split(" ")
+        words = line.rstrip().split()
         for word in words:
-                word_count[word] = word_count.get(word, 0) + 1
+            word_count[word] = word_count.get(word, 0) + 1
+
+    for word, count in word_count.items():
+        print(f"{word} {count}")
 
     return word_count
 
 with open("twain.txt") as input_file:
-    print(count_words(input_file))
+    count_words(input_file)
 
